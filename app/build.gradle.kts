d55b86ed6703fb14ff497bc8103c2843f1af1c2b
Zaur <zaur.ish005@gmail.com>
2024-06-07 11:39:41 +0300

    config project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 group = "hexlet.code"
@@ -17,3 +18,5 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+application { mainClass.set("hexlet.code.App") }
